3260742eb5b8fee1017d7e781aa6cefe0d7bf411
garima.thakore <garima.thakore@IBBDPC2239>
2019-01-03 14:37:43 +0530

    chage in CAF js

--- CR/APP Form/js/CAF.js
+++ CR/APP Form/js/CAF.js
@@ -22,29 +22,6 @@ var svcFileName = 'SPINHelper_IB_WebService.svc';
 var globalCountry = '';
 
 
-//var inputQuantity = [];
-//$(function () {
-//    $(".yearGraduated").each(function (i) {
-//        inputQuantity[i] = this.defaultValue;
-//        $(this).data("idx", i); // save this field's index to access later
-//    });
-//    $(".yearGraduated").on("keyup", function (e) {
-//        var $field = $(this),
-//            val = this.value,
-//            $thisIndex = parseInt($field.data("idx"), 10); // retrieve the index
-//        //        window.console && console.log($field.is(":invalid"));
-//        //  $field.is(":invalid") is for Safari, it must be the last to not error in IE8
-//        if (this.validity && this.validity.badInput || isNaN(val) || $field.is(":invalid")) {
-//            this.value = inputQuantity[$thisIndex];
-//            return;
-//        }
-//        if (val.length > Number($field.attr("maxlength"))) {
-//            val = val.slice(0, 4);
-//            $field.val(val);
-//        }
-//        inputQuantity[$thisIndex] = val;
-//    });
-//});
 
 $(document).ready(function () {
 
